3c98ce4f397f7ce1c8353f829e53664b17a35309
idOgItt <sergeylkd@bk.ru>
2025-01-08 21:37:59 +0300

    backup и front - теперь правильный

--- backup/docker-entrypoint.sh
+++ backup/docker-entrypoint.sh
@@ -7,7 +7,7 @@ WAIT_TIMEOUT=${WAIT_TIMEOUT:-30}
 
 echo "Waiting for PostgreSQL at ${DB_HOST}:${DB_PORT} for up to ${WAIT_TIMEOUT} seconds..."
 
-/wait-for-it.sh "${DB_HOST}:${DB_PORT}" -t "${WAIT_TIMEOUT}"
+wait-for-it.sh "${DB_HOST}:${DB_PORT}" -t "${WAIT_TIMEOUT}"
 
 echo "Database is up. Starting the backup service..."
 
